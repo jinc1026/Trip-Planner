8089c5d435856e9f889acc3734597a523b20fc46
Jin Choe <jinc1026@hotmail.com>
2022-03-18 20:37:42 +0000

    All functionality and css added

--- website/app.js
+++ website/app.js
@@ -8,7 +8,10 @@ const weatherBitBaseUrlCurrent ="https://api.weatherbit.io/v2.0/current?lat=";
 const weatherBitBaseUrlForecast = "https://api.weatherbit.io/v2.0/forecast/daily?lat=";
 const weatherBitKey = "&key=c086327b5e2345ddb6a22055e7e275a8";
 
-// const geonamesKey = "&username=jinc1026";
+const geonamesKey = "&username=jinc1026";
+
+const pixabayUrl = "https://pixabay.com/api/?";
+const pixabayKey = "key=26197022-380369692144c45e27439b513";
 
 
 // Create a new date instance dynamically with JS
@@ -39,7 +42,6 @@ async function weatherInfoStartCallBack(){
 	if (coordinates[0] !== undefined){
 		if(departureDate === ""){
 			document.getElementById("error").innerHTML = "Please set your departure date correctly";
-			clearUIdata();
 		} else {
 			if (diffDays<7){
 				const weather = await getWeatherInfo(coordinates[0]);
@@ -51,12 +53,12 @@ async function weatherInfoStartCallBack(){
 					country: weather.data[0].country_code,
 					weather: weather.data[0].weather.description,
 					temp: weather.data[0].temp,
-					precip: weather.data[0].precip,
+					// precip: Math.round(weather.data[0].precip*10)/10,
 					rh: Math.round(weather.data[0].rh*10)/10,
 					wind_spd: Math.round(weather.data[0].wind_spd*10)/10,
 				}).then(function(postResponse){
 						// Retreive the most recent weather info and upate UI
-							updateUI();
+							createSchedule();
 				});
 
 			} else {
@@ -74,27 +76,28 @@ async function weatherInfoStartCallBack(){
 					rh: Math.round(weather.data[0].rh*10)/10,
 					wind_spd: Math.round(weather.data[0].wind_spd*10)/10,
 				}).then(function(postResponse){
-						// Retreive the most recent weather info and upate UI
-							updateUI();
+						// Retreive the most recent weather info and add into the page
+						createSchedule();
 				});			
 			}
 		}
-
 	}
+	
+	// Clear input values
+	document.getElementById("location").value = "";
+	document.getElementById("departureDate").value = "";	
 };
 
 const getCoordinateInfo = async function(location){
-	const coordinateResponse = await fetch(openWeatherCoordinateUrl + location + apiKey);
-
-	if (!coordinateResponse.ok) {
-		//Things to be done if the response is bad
-		document.getElementById("error").innerHTML = "Incorrect city name! Please double check the city name";
-		clearUIdata();
-		throw new Error("Bad response from server. Fail to get coordinate info");
-	};
-
 	try {
+		const coordinateResponse = await fetch(openWeatherCoordinateUrl + location + apiKey);
 		const coordinateData = await coordinateResponse.json();
+		if (!coordinateResponse.ok || coordinateData.length === 0) {
+			//Things to be done if the response is bad
+			document.getElementById("error").innerHTML = "Incorrect city name! Please double check the city name";
+			throw new Error("Bad response from server. Fail to get coordinate info");
+		};
+
 		return coordinateData;
 	} catch(error) {
 		console.log(`Error: ${error}`);
@@ -155,33 +158,57 @@ const postWeatherInfo = async function(url="", data={}){
 	}
 };
 
-const updateUI = async function(){
-	const request = await fetch("/recent");
-
+const createSchedule = async function(){	
 	try {
+		const request = await fetch("/recent");
 		const recentEntry = await request.json();
-		console.log(recentEntry);
-		document.getElementById("date").innerHTML = `Departure Date: ${recentEntry.date}`;
-		document.getElementById("city").innerHTML = `City: ${recentEntry.city}`;
-		document.getElementById("country").innerHTML = `Country: ${recentEntry.country}`;
-		document.getElementById("weather").innerHTML = `Weather: ${recentEntry.weather}`;
-		document.getElementById("temp").innerHTML = `Temperature(C): ${recentEntry.temp}`;
-		document.getElementById("precip").innerHTML = `Precipitation(mm/hr): ${recentEntry.precip}`;
-		document.getElementById("rh").innerHTML = `Relative Humidity(%): ${recentEntry.rh}`;
-		document.getElementById("wind_spd").innerHTML = `Wind Speed(m/s): ${recentEntry.wind_spd}`;
+		
+		const imageRequest = await fetch(pixabayUrl + pixabayKey + "&q=" + recentEntry.city +"&image_type=photo&orientation=horizontal");
+		const imageList = await imageRequest.json();
+		const imageUrl = imageList.hits[0].webformatURL;		
+		
+		let newEntry = document.createElement('div');
+		let newImage = document.createElement('img');
+		let newInfo = document.createElement('div');
+		newEntry.classList.add("entry");
+		newInfo.classList.add("weatherInfo");
+		
+		newImage.setAttribute('src', imageUrl);
+		newImage.setAttribute('alt', recentEntry.city);
+		newImage.classList.add("cityImage");
+		newEntry.appendChild(newImage);
+		
+		let dateDiv = document.createElement('div')
+		let locationDiv = document.createElement('div');
+		let weatherDiv = document.createElement('div');
+		let tempDiv = document.createElement('div');
+		let rhDiv = document.createElement('div');
+		
+		dateDiv.innerHTML = `Departure Date: ${recentEntry.date}`;
+		locationDiv.innerHTML = `Location: ${recentEntry.city}, ${recentEntry.country}`;
+		weatherDiv.innerHTML = `Weather: ${recentEntry.weather}`;
+		tempDiv.innerHTML = `Temperature(C): ${recentEntry.temp}`;
+		rhDiv.innerHTML = `Relative Humidity(%): ${recentEntry.rh}`;
+		
+		newInfo.appendChild(dateDiv);
+		newInfo.appendChild(locationDiv);
+		newInfo.appendChild(weatherDiv);
+		newInfo.appendChild(tempDiv);
+		newInfo.appendChild(rhDiv);
+		
+		let newDelete = document.createElement('button');
+		newDelete.classList.add('deleteBtn', 'btn', 'btn-danger');
+		newDelete.innerHTML = "Delete";
+		newDelete.addEventListener('click', function(){
+			this.parentNode.parentNode.remove();
+		});
+		newInfo.appendChild(newDelete);
+		
+		newEntry.appendChild(newInfo);
+		document.getElementById('schedule').appendChild(newEntry);
 		document.getElementById("error").innerHTML = "";
-	} catch (error) {
-		console.log("Error", error);
+	} catch (error){
+		console.log("Error: ", error);
 	}
-}
-
-const clearUIdata = function(){
-	document.getElementById("date").innerHTML = "";
-	document.getElementById("city").innerHTML = "";
-	document.getElementById("country").innerHTML = "";
-	document.getElementById("weather").innerHTML = "";
-	document.getElementById("temp").innerHTML = "";
-	document.getElementById("precip").innerHTML = "";
-	document.getElementById("rh").innerHTML = "";
-	document.getElementById("wind_spd").innerHTML = "";
+	
 }
